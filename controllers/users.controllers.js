9d66db7d055a07638f5206c8104bc37fd0fe2dcb
kusenabdullah-123 <kusena322@gmail.com>
2020-06-02 21:20:43 +0700

    menambah autentikasi login

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,12 +1,21 @@
 const router = require("express").Router();
 const { getUsers } = require("../model/users/user.service");
-router.get("/users", async (req, res) => {
+const jwt = require("jsonwebtoken");
+router.post("/", async (req, res) => {
   try {
-    const data = await getUsers({ user: "kusen", pass: "kusen" });
-    res.status(200).json({
-      status: 1,
-      result: data[0],
-    });
+    const { username, password } = req.body;
+    const user = await getUsers({ user: username, pass: password });
+    user.length > 0
+      ? jwt.sign({ user }, process.env.KEY_SECRET, (err, token) => {
+          res.json({
+            status: 1,
+            token,
+          });
+        })
+      : res.status(200).json({
+          status: 1,
+          result: "not found",
+        });
   } catch (error) {
     res.status(500);
   }
--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const users = require("./model/users/users.controllers");
+const users = require("./controllers/users.controllers");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
-app.use(users);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const vertify = (req, res, next) => {
+  const { auth } = req.headers;
+  if (typeof auth == "undefined") {
+    res.status(203);
+  }
+  req.token = auth;
+  next();
+};
+app.use("/api/login", users);
+
+app.get("/barang", vertify, (req, res) => {
+  try {
+    const { user } = jwt.verify(req.token, process.env.KEY_SECRET);
+    res.end();
+  } catch (error) {
+    res.status(203).json({
+      message: "invalid token",
+    });
+  }
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`http://localhost:${process.env.PORT}`);
 });
